3a0de3d6f1094de30fa44a373e0ac79e60a7e851
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-09-20 15:23:01 +0700

    Fix: Change ConnectionString to JSON File

--- HouseFinder_API/HouseFinder_API/Models/FUHouseFinderContext.cs
+++ HouseFinder_API/HouseFinder_API/Models/FUHouseFinderContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 #nullable disable
 
@@ -35,8 +36,9 @@ namespace HouseFinder_API.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=MSI\\SQLEXPRESS;database=FUHouseFinder;uid=sa;pwd=sa");
+                //Read JSON File -> ConnectionString
+                var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+                optionsBuilder.UseSqlServer(config.GetConnectionString("DBContext"));
             }
         }
 
