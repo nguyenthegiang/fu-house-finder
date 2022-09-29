f0a6f779bdb8b6e597cf72d121248d3092368bd2
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-09-29 10:29:53 +0700

    delete fake rotuting

--- house-finder-client/src/app/app.module.ts
+++ house-finder-client/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
