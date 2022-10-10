a87e77a0ebfdef8d675d23f5aeb76a11396ed6a2
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-10-10 10:31:46 +0700

    Create House Controller

---
+++ HouseFinder_API/HouseFinder_API/Controllers/HousesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.IRepository;
+using BusinessObjects;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Repositories.Repositories;
+
+namespace HouseFinder_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HousesController : ControllerBase
+    {
+        private IHousesRepository housesRepository = new HouseRepository();
+
+        //GET: api/Houses
+        [HttpGet]
+        public ActionResult<IEnumerable<House>> GetAllHouses() => housesRepository.GetAllHouses();
+
+    }
+}
