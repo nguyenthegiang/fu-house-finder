d7546d88f03bc8f3ed141b05aef295774ea2fe6c
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-10-11 10:54:27 +0700

    Create UserController.cs

---
+++ HouseFinder_API/HouseFinder_API/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.IRepository;
+using Repositories.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseFinder_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserReposiotry userReposiotry = new UserRepository();
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(string id)
+        {
+            UserDTO userDTO = userReposiotry.GetUserByID(id);
+            if (userDTO == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(userDTO);
+            }
+        }
+    }
+}
