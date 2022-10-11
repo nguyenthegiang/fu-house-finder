0e83dd0ba0b85c724a3a8400b7c65d5a5631bbdf
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-10-11 10:38:33 +0700

    Create RoomsController.cs

---
+++ HouseFinder_API/HouseFinder_API/Controllers/RoomsController.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.IRepository;
+using Repositories.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseFinder_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        private IRoomsRepository roomsRepository = new RoomRepository();
+        [HttpGet("{id}")]
+        public IActionResult GetRoomByHouseId(int id)
+        {
+            List<RoomDTO> roomsDTO = roomsRepository.GetRoomsByHouseId(id);
+            if (roomsDTO == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(roomsDTO);
+            }
+        }
+    }
+}
