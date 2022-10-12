12181e49937b42c950f3add49cade658db6531a8
huyenbnhe150346 <huyenbnhe150346@fpt.edu.vn>
2022-10-13 01:05:30 +0700

    [Angular] Get liist available rooms of a house

--- house-finder-client/src/app/services/room.service.ts
+++ house-finder-client/src/app/services/room.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Room } from '../models/room';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
 
-  constructor() { }
+  readonly APIUrl = "https://localhost:5001/api/Rooms";
+
+  constructor(private http: HttpClient) {}
+
+  //[House Detail] Get List Available Rooms by HouseId
+  getAvailableRooms(houseId: number): Observable<any[]> {
+    return this.http.get<any[]>(this.APIUrl + "/getAvailableRooms?HouseId=" + houseId);
+  }
 }
