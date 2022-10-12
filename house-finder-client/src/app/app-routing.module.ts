b8e8685ee3c30b0c6d6d81636de6fa2bab609a30
An Nguyen <annt@meta-vi.vn>
2022-10-12 17:27:11 +0700

    login fb gg angular

--- house-finder-client/src/app/app-routing.module.ts
+++ house-finder-client/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HouseDetailComponent} from './house-detail/house-detail.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'home', component: HomepageComponent },
-  { path: 'house-detail', component: HouseDetailComponent}
-
+  { path: 'house-detail', component: HouseDetailComponent},
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- house-finder-client/src/app/app.module.ts
+++ house-finder-client/src/app/app.module.ts
@@ -1,25 +1,54 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HouseDetailComponent } from './house-detail/house-detail.component';
+import { LoginComponent } from './login/login.component';
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from 'angularx-social-login';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    HouseDetailComponent
+    HouseDetailComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(
+            'clientId'
+          )
+        },
+        {
+          id: FacebookLoginProvider.PROVIDER_ID,
+          provider: new FacebookLoginProvider('790258838897169')
+        }
+      ],
+      onError: (err) => {
+        console.error(err);
+      }
+    } as SocialAuthServiceConfig,
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
