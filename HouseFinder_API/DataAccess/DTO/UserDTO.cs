4ed8e8b105c408c85f405871e18fe8627491a4f6
Nguyen Thu An <42872661+annthe150432@users.noreply.github.com>
2022-10-13 17:17:48 +0700

    login api

--- HouseFinder_API/DataAccess/DTO/UserDTO.cs
+++ HouseFinder_API/DataAccess/DTO/UserDTO.cs
@@ -30,5 +30,46 @@ namespace DataAccess.DTO
 
         //Address of this User
         public virtual AddressDTO Address { get; set; }
+        public virtual RoleDTO Role { get; set; }
+    }
+
+    public class RegisterDTO
+    {
+        public string FacebookUserId { get; set; }
+        public string GoogleUserId { get; set; }
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+        public int? RoleId { get; set; }
+    }
+    public class LoginDTO
+    {
+        public string FacebookUserId { get; set; } = null;
+        public string GoogleUserId { get; set; } = null;
+        public string Email { get; set; } = null;
+        public string Password { get; set; } = null;
+    }
+    public class ResponseDTO
+    {
+        public string UserId { get; set; }
+        public string FacebookUserId { get; set; }
+        public string GoogleUserId { get; set; }
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+        public bool? Active { get; set; }
+        public string ProfileImageLink { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FacebookUrl { get; set; }
+        public string IdentityCardFrontSideImageLink { get; set; }
+        public string IdentityCardBackSideImageLink { get; set; }
+        public int? AddressId { get; set; }
+        public int? RoleId { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string LastModifiedBy { get; set; }
+
+        //Address of this User
+        public virtual AddressDTO Address { get; set; }
+        public virtual RoleDTO Role { get; set; }
     }
 }
---
+++ HouseFinder_API/HouseFinder_API/Authentication/IAuthentication.cs
@@ -0,0 +1,14 @@
+﻿using BusinessObjects;
+using DataAccess.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseFinder_API.Authentication
+{
+    public interface IAuthentication
+    {
+        public string Authenticate(ResponseDTO user);
+    }
+}
