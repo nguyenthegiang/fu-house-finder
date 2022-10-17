793ad43ad833c87c7ed212ef4f6e7c3b19233db2
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-10-17 11:45:22 +0700

    Create LandLordInfomationController.cs

---
+++ HouseFinder_API/HouseFinder_API/Controllers/LandLordInfomationController.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.IRepository;
+using Repositories.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseFinder_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LandLordInfomationController : ControllerBase
+    {
+        private ILandlordInfomationRepository landlordInfomationRepository = new LandlordInfomationRepository();
+        //GET: api/Hoses/LandlordInfo?LandlordId=
+        [HttpGet("LandlordInfo")]
+        public IActionResult GetLandlordInfomationByLandlordId(string LandlordId)
+        {
+            LandlordDasboardInformationDTO l = landlordInfomationRepository.GetLandLordInfomationByLandlordId(LandlordId);
+            if (l == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(l);
+            }
+        }
+    }
+}
