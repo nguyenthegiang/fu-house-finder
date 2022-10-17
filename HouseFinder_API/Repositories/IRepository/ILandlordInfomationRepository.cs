2dea07e4c3da44def51d2bb6d833829ce0a192ad
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-10-17 11:45:27 +0700

    Create ILandlordInfomationRepository.cs

---
+++ HouseFinder_API/Repositories/IRepository/ILandlordInfomationRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.IRepository
+{
+    public interface ILandlordInfomationRepository
+    {
+        public LandlordDasboardInformationDTO GetLandLordInfomationByLandlordId(string landlordId);
+    }
+}
