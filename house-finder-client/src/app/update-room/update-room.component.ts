a55c9513f18da2a83d82de81b056604b4f015ae4
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-10-24 17:41:38 +0700

    Update update-room.component.ts

--- house-finder-client/src/app/update-room/update-room.component.ts
+++ house-finder-client/src/app/update-room/update-room.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Room } from '../models/room';
+import { RoomService } from '../services/room.service';
 
 @Component({
   selector: 'app-update-room',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-room.component.scss']
 })
 export class UpdateRoomComponent implements OnInit {
+  //Infomation of this room
+  roomDetail: Room | undefined;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private roomService: RoomService
+  ) { }
 
   ngOnInit(): void {
+  }
+  getRoomByRoomId() {
+    //Get id of room from router
+    const roomId = Number(this.route.snapshot.paramMap.get('roomId'));
+    this.roomService.getRoomByRoomId(roomId).subscribe(data => {
+      this.roomDetail = data;
+    })
   }
 
 }
