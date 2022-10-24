7e8719166746d3470c7bef8bfaac82fa70ad1dfe
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-10-24 17:19:09 +0700

    Update room.service.ts

--- house-finder-client/src/app/services/room.service.ts
+++ house-finder-client/src/app/services/room.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Room } from '../models/room';
@@ -10,7 +10,11 @@ export class RoomService {
 
   readonly APIUrl = "https://localhost:5001/api/Rooms";
 
-  constructor(private http: HttpClient) {}
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
 
   //[House Detail] Get List Available Rooms by HouseId
   getAvailableRooms(houseId: number): Observable<any[]> {
@@ -18,7 +22,28 @@ export class RoomService {
   }
 
   //[Landlord: List Room] Get List of Rooms by HouseId
-  getRooms(houseId: number) : Observable<any[]>{
+  getRooms(houseId: number): Observable<any[]> {
     return this.http.get<any[]>(this.APIUrl + "/getByHouseId?HouseId=" + houseId);
   }
+
+  //[Landlord: Create Room]
+  addRoom(room: Room): Observable<any> {
+    return this.http.post<any>(this.APIUrl, room, this.httpOptions);
+  }
+
+  //[Landlord: Update Room]
+  updateRoom(room: Room): Observable<any> {
+    return this.http.put<any>(this.APIUrl, room, this.httpOptions);
+  }
+
+  //[Landlord: Delete Room]
+  deleteRoom(roomId: number): Observable<any> {
+    return this.http.delete<any>(this.APIUrl + "/Rooms?roomId=" + roomId, this.httpOptions);
+  }
+
+  //[Landlord: Get Room] Get room by roomId
+  getRoom(roomId: number): Observable<Room> {
+    return this.http.get<Room>(this.APIUrl + "/Rooms?roomId=" + roomId);
+  }
 }
+
