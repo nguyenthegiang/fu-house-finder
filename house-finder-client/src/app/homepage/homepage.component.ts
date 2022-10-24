30aa6bbb937e54efd92dea061073483eeda0f9c2
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-10-24 17:28:40 +0700

    [Angular] (Home Page) Display List Room Type
    {HuyenBN}

--- house-finder-client/src/app/homepage/homepage.component.ts
+++ house-finder-client/src/app/homepage/homepage.component.ts
@@ -1,6 +1,8 @@
 import { HouseService } from './../services/house.service';
 import { Component, OnInit } from '@angular/core';
 import { House } from '../models/house';
+import { RoomType } from '../models/roomType';
+import { RoomTypeService } from '../services/room-type.service';
 
 @Component({
   selector: 'app-homepage',
@@ -14,18 +16,28 @@ export class HomepageComponent implements OnInit {
   //{Search} input value
   searchHouseName: any;
 
-  constructor(private houseService: HouseService) { }
+  //List of room types
+  roomTypes: RoomType[] = [];
+
+  constructor(
+    private houseService: HouseService,
+    private roomTypeService: RoomTypeService,
+  ) { }
 
   ngOnInit(): void {
     //Get List of all Houses
     this.houseService.getAllHouses().subscribe(data => {
       this.houses = data;
     });
+
+    //Call API: get Room types
+    this.roomTypeService.getRoomTypes().subscribe(data => {
+      this.roomTypes = data;
+    })
   }
 
   //Search House by Name
-  searchHouseByName()
-  {
+  searchHouseByName() {
     var value = this.searchHouseName;
     //call API
     this.houseService.searchHouseByName(value).subscribe(data => {
