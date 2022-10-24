ee87d79bab410af1e99e930097124c3dffbec721
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-10-24 16:14:02 +0700

    Update IRoomsRepository.cs

--- HouseFinder_API/Repositories/IRepository/IRoomsRepository.cs
+++ HouseFinder_API/Repositories/IRepository/IRoomsRepository.cs
@@ -15,5 +15,7 @@ namespace Repositories.IRepository
         public List<RoomDTO> GetAvailableRoomsByHouseId(int houseId);
 
         public void UpdateRoomByRoomId(Room room);
-     }
+        public void CreateRoom(Room room);
+        public void DeleteRoom(int roomId);
+    }
 }
