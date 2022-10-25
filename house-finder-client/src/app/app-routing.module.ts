0ed289913344a967a0346a6fe39acd8e625dd7dd
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-10-25 15:58:41 +0700

    Update app-routing.module.ts

--- house-finder-client/src/app/app-routing.module.ts
+++ house-finder-client/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { HomepageComponent } from './homepage/homepage.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HouseDetailComponent} from './house-detail/house-detail.component';
+import { HouseDetailComponent } from './house-detail/house-detail.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './Landlord/dashboard/dashboard.component';
 import { ListRoomComponent } from './Landlord/list-room/list-room.component';
@@ -10,20 +10,22 @@ import { ListLandlordComponent } from './Staff/list-landlord/list-landlord.compo
 import { ListReportComponent } from './Staff/list-report/list-report.component';
 import { ListHouseComponent } from './Staff/list-house/list-house.component';
 import { ListLandlordSignupRequestComponent } from './Staff/list-landlord-signup-request/list-landlord-signup-request.component';
+import { UpdateRoomComponent } from './Landlord/update-room/update-room.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent },
-  { path: 'login', component: LoginComponent},
-  { path: 'house-detail/:id', component: HouseDetailComponent},
-  { path: 'house-detail', component: HouseDetailComponent},
-  { path: 'Landlord/dashboard', component: DashboardComponent},
-  { path: 'Landlord/list-room/:id', component: ListRoomComponent},
-  { path: 'Staff/dashboard', component: DashboardStaffComponent},
-  { path: 'Staff/list-landlord', component: ListLandlordComponent},
-  { path: 'Staff/list-report', component: ListReportComponent},
-  { path: 'Staff/list-house', component: ListHouseComponent},
-  { path: 'Staff/list-signup', component: ListLandlordSignupRequestComponent}
+  { path: 'login', component: LoginComponent },
+  { path: 'house-detail/:id', component: HouseDetailComponent },
+  { path: 'house-detail', component: HouseDetailComponent },
+  { path: 'Landlord/dashboard', component: DashboardComponent },
+  { path: 'Landlord/list-room/:id', component: ListRoomComponent },
+  { path: 'Staff/dashboard', component: DashboardStaffComponent },
+  { path: 'Staff/list-landlord', component: ListLandlordComponent },
+  { path: 'Staff/list-report', component: ListReportComponent },
+  { path: 'Staff/list-house', component: ListHouseComponent },
+  { path: 'Staff/list-signup', component: ListLandlordSignupRequestComponent },
+  { path: 'Landlord/update-room/1', component: UpdateRoomComponent }
 ];
 
 @NgModule({
@@ -31,5 +33,5 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 
-export class AppRoutingModule {}
+export class AppRoutingModule { }
 export const routingComponents = [HomepageComponent]
