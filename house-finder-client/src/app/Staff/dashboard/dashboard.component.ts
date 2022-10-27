2c5ae4119ca0af3d0c42deb136b7d39e0a931663
huyenbnhe150346 <huyenbnhe150346@fpt.edu.vn>
2022-10-27 17:16:47 +0700

    [Angular] Display total of available rooms

--- house-finder-client/src/app/Staff/dashboard/dashboard.component.ts
+++ house-finder-client/src/app/Staff/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HouseService } from 'src/app/services/house.service';
+import { RoomTypeService } from 'src/app/services/room-type.service';
+import { RoomService } from 'src/app/services/room.service';
+import { StatusService } from 'src/app/services/status.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardStaffComponent implements OnInit {
+  //Total of available rooms
+  availabelRoomsNum: number = 0;
 
-  constructor() { }
+  constructor(
+    private roomService: RoomService,
+    private houseService: HouseService,
+    private roomTypeService: RoomTypeService,
+    private statusService: StatusService,
+  ) { }
 
   ngOnInit(): void {
+    //Call API: get total of available rooms
+    this.roomService.countAvailableRooms().subscribe(data => {
+      this.availabelRoomsNum = data;
+    });
   }
 
 }
