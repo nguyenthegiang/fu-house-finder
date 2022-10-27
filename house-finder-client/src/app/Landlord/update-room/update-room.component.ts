864a18bcfa4a3cc4600fcac43cf83233821a9118
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-10-27 11:40:50 +0700

    Update update-room.component.ts

--- house-finder-client/src/app/Landlord/update-room/update-room.component.ts
+++ house-finder-client/src/app/Landlord/update-room/update-room.component.ts
@@ -2,6 +2,10 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
 import { ActivatedRoute } from '@angular/router';
 import { Room } from 'src/app/models/room';
 import { RoomService } from 'src/app/services/room.service';
+//import { StatusService } from 'src/app/services/status.service';
+//import { Status } from 'src/app/models/status';
+
+
 
 @Component({
   selector: 'app-update-room',
@@ -11,9 +15,15 @@ import { RoomService } from 'src/app/services/room.service';
 export class UpdateRoomComponent implements OnInit, OnChanges {
   //Infomation of this room
   roomDetail: Room | undefined;
+  //listStatus: Status[] = [];
+  //test string
+  statusSelected: string = "";
 
   constructor(
-    private roomService: RoomService
+    private roomService: RoomService,
+    //demo status selected
+    //private statusService: StatusService,
+
   ) { }
 
   @Input() roomId!: number;
@@ -23,6 +33,10 @@ export class UpdateRoomComponent implements OnInit, OnChanges {
 
   // When @Input() roomId changes => user has clicked on 'Update' button
   ngOnChanges(changes: SimpleChanges): void {
+
+    // status demo
+    //this.getAllStatus();
+
     //call API to get detail info of room
     this.getRoomByRoomId();
   }
@@ -36,7 +50,15 @@ export class UpdateRoomComponent implements OnInit, OnChanges {
 
     this.roomService.getRoomByRoomId(this.roomId).subscribe(data => {
       this.roomDetail = data;
-    })
+      //demo selected
+      //this.statusSelected = this.roomDetail.status.statusName;
+
+    });
   }
 
+  // getAllStatus() {
+  //   this.statusService.getAllStatus().subscribe(data => {
+  //     this.listStatus = data;
+  //   })
+  // }
 }
