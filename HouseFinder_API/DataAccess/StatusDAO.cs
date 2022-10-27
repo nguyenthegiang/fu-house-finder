3d310e10d5f4591d2e3eda71d9d617c902b8397c
ThongVipPro <84122855+ThongVipPro@users.noreply.github.com>
2022-10-27 11:40:57 +0700

    Update StatusDAO.cs

--- HouseFinder_API/DataAccess/StatusDAO.cs
+++ HouseFinder_API/DataAccess/StatusDAO.cs
@@ -1,4 +1,7 @@
-﻿using DataAccess.DTO;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using BusinessObjects;
+using DataAccess.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,5 +34,26 @@ namespace DataAccess
 
             return statusesOfHouse;
         }
+        //Get All List Status
+        public static List<StatusDTO> GetAllListStatus()
+        {
+            List<StatusDTO> statusDTOs = new List<StatusDTO>();
+            try
+            {
+                using (var context = new FUHouseFinderContext())
+                {
+                    //Get by Id, include Address
+                    MapperConfiguration config;
+                    config = new MapperConfiguration(cfg => cfg.AddProfile(new MapperProfile()));
+                    statusDTOs = context.Statuses.ProjectTo<StatusDTO>(config).ToList();
+                    return statusDTOs;
+                }
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
     }
 }
