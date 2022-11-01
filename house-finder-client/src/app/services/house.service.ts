fd86a5339ece7d384afbac2be0a91c9dc8933559
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-11-01 16:47:50 +0700

    [Angular] (Home Page) List House: Filter OData
    Filter by Price

--- house-finder-client/src/app/services/house.service.ts
+++ house-finder-client/src/app/services/house.service.ts
@@ -30,6 +30,8 @@ export class HouseService {
     pageNumber: number,
     searchName?: string,
     campusId?: number,
+    maxPrice?: number,
+    minPrice?: number,
   ): Observable<House[]> {
     //define API here to append query options into it later
     var filterAPIUrl = this.APIUrl + `/availableHouses?`;
@@ -40,7 +42,7 @@ export class HouseService {
     filterAPIUrl += `$skip=${skip}&$top=${top}`;
 
     //[Filter] check if user has at least 1 filter
-    if (searchName || campusId) {
+    if (searchName || campusId || maxPrice) {
       //add filter to API
       filterAPIUrl += `&$filter=`;
     }
@@ -70,8 +72,21 @@ export class HouseService {
         //if this one is the first filter -> mark it so others won't add 'and'
         checkFirstFilter = false;
       }
-      
+
       filterAPIUrl += `CampusId eq ${campusId}`;
+    }
+
+    //[Filter] add filter by room price if has
+    if (maxPrice != undefined && minPrice != undefined) {
+      //if is not the first filter -> need to add 'and' to API URL
+      if (!checkFirstFilter) {
+        filterAPIUrl += ` and `;
+      } else {
+        //if this one is the first filter -> mark it so others won't add 'and'
+        checkFirstFilter = false;
+      }
+
+      filterAPIUrl += `LowestRoomPrice le ${maxPrice} and HighestRoomPrice ge ${minPrice}`;
     }
 
     return this.http.get<House[]>(filterAPIUrl);
