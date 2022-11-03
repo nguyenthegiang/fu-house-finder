5c2336ba24af11a68da5853d24589b6e2b956d99
huyenbnhe150346 <huyenbnhe150346@fpt.edu.vn>
2022-11-03 19:35:21 +0700

    Get total order of the current month

--- HouseFinder_API/DataAccess/DTO/OrderDAO.cs
+++ HouseFinder_API/DataAccess/DTO/OrderDAO.cs
@@ -19,9 +19,38 @@ namespace DataAccess.DTO
                 using (var context = new FUHouseFinderContext())
                 {
                     //Convert month type int to string
-                    string monthName = DateTimeFormatInfo.CurrentInfo.GetAbbreviatedMonthName(month); ;
+                    string monthName = DateTimeFormatInfo.CurrentInfo.GetAbbreviatedMonthName(month);
                     //Get list orders by month
                     List<Order> orders = context.Orders.Where(o => o.OrderedDate.ToString("MMMM dd").Contains(monthName)).ToList();
+                    //Count number of orders
+                    orders.Count();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return total;
+        }
+
+
+        //[Staff/Dashboard] Get total order of the current month
+        public int GetTotalOrderCurrentMonth()
+        {
+            int total = 0;
+            try
+            {
+                using (var context = new FUHouseFinderContext())
+                {
+                    //Get current date
+                    DateTime now = DateTime.Now;
+                    //Get the start date of the current month
+                    var startDate = new DateTime(now.Year, now.Month, 1);
+                    //Get end date of the current month
+                    var endDate = startDate.AddMonths(1).AddDays(-1);
+
+                    //Get list orders current month
+                    List<Order> orders = context.Orders.Where(o => o.OrderedDate >= startDate).Where(o => o.OrderedDate <= endDate).ToList();
                     //Count number of orders
                     orders.Count();
                 }
