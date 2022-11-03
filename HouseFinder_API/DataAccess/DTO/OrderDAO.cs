e02e7f250ea07ab0a799fcdba67b084756f77937
huyenbnhe150346 <huyenbnhe150346@fpt.edu.vn>
2022-11-03 19:08:10 +0700

    Update database

---
+++ HouseFinder_API/DataAccess/DTO/OrderDAO.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DTO
+{
+    public class OrderDAO
+    {
+        public int GetTotalOrderByMonth(int month)
+        {
+            int total = 0;
+            try
+            {
+                using (var context = new FUHouseFinderContext())
+                {
+                    //string monthName = "";
+                    ////Get list orders by month
+                    ////List<Order> orders = context.Orders.Where(o => o.OrderedDate.ToString("MMMM dd").Contains(monthName)).ToList();
+                    //List<Order> orders = context.Orders.ToList();
+                    //foreach (Order o in orders)
+                    //{
+                    //    if( o.OrderedDate != null)
+                    //    {
+                    //        String tday = DateTime.Now.ToString("MM/dd/yyyy");
+                    //    }
+                    //}
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return total;
+        }
+    }
+}
---
+++ HouseFinder_API/DataAccess/DTO/OrderDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DTO
+{
+    public class OrderDTO
+    {
+        public int OrderId { get; set; }
+        public string StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string OrderContent { get; set; }
+        public bool? Solved { get; set; }
+        public DateTime? OrderedDate { get; set; }
+        public DateTime? SolvedDate { get; set; }
+
+    }
+}
