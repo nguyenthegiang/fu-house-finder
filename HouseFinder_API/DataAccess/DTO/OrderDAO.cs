7c1f56a9d8fe21c24d7b39b77373d84709d378d3
huyenbnhe150346 <huyenbnhe150346@fpt.edu.vn>
2022-11-03 19:18:55 +0700

    Get total order by month

--- HouseFinder_API/DataAccess/DTO/OrderDAO.cs
+++ HouseFinder_API/DataAccess/DTO/OrderDAO.cs
@@ -2,6 +2,7 @@
 using BusinessObjects;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,17 +18,12 @@ namespace DataAccess.DTO
             {
                 using (var context = new FUHouseFinderContext())
                 {
-                    //string monthName = "";
-                    ////Get list orders by month
-                    ////List<Order> orders = context.Orders.Where(o => o.OrderedDate.ToString("MMMM dd").Contains(monthName)).ToList();
-                    //List<Order> orders = context.Orders.ToList();
-                    //foreach (Order o in orders)
-                    //{
-                    //    if( o.OrderedDate != null)
-                    //    {
-                    //        String tday = DateTime.Now.ToString("MM/dd/yyyy");
-                    //    }
-                    //}
+                    //Convert month type int to string
+                    string monthName = DateTimeFormatInfo.CurrentInfo.GetAbbreviatedMonthName(month); ;
+                    //Get list orders by month
+                    List<Order> orders = context.Orders.Where(o => o.OrderedDate.ToString("MMMM dd").Contains(monthName)).ToList();
+                    //Count number of orders
+                    orders.Count();
                 }
             }
             catch (Exception e)
