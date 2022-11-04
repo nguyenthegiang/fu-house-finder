973f9a4170109a2eedc0bb1068b158a254f6f6cf
kienntfs1013 <63889377+kienntfs1013@users.noreply.github.com>
2022-11-04 22:14:38 +0700

    add rate house component

---
+++ house-finder-client/src/app/Landlord/rate-house/rate-house.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rate-house',
+  templateUrl: './rate-house.component.html',
+  styleUrls: ['./rate-house.component.scss']
+})
+export class RateHouseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- house-finder-client/src/app/app.module.ts
+++ house-finder-client/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { RoomDetailComponent } from './Guest/room-detail/room-detail.component';
 import { AddHouseComponent } from './Landlord/add-house/add-house.component';
 import { UpdateHouseComponent } from './Landlord/update-house/update-house.component';
 import { LandlordHouseDetailComponent } from './Landlord/landlord-house-detail/landlord-house-detail.component';
+import { RateHouseComponent } from './Landlord/rate-house/rate-house.component';
 
 @NgModule({
   declarations: [
@@ -46,6 +47,7 @@ import { LandlordHouseDetailComponent } from './Landlord/landlord-house-detail/l
     AddHouseComponent,
     UpdateHouseComponent,
     LandlordHouseDetailComponent,
+    RateHouseComponent,
   ],
   imports: [
     BrowserModule,
