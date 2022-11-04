f215081a52d6f8480a33838da8ca78daf6170223
huyenbnhe150346 <huyenbnhe150346@fpt.edu.vn>
2022-11-05 01:27:45 +0700

    Add chart to dashboard

--- house-finder-client/src/app/Staff/dashboard/dashboard.component.ts
+++ house-finder-client/src/app/Staff/dashboard/dashboard.component.ts
@@ -3,6 +3,9 @@ import { HouseService } from 'src/app/services/house.service';
 import { RoomTypeService } from 'src/app/services/room-type.service';
 import { RoomService } from 'src/app/services/room.service';
 import { StatusService } from 'src/app/services/status.service';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+
 
 @Component({
   selector: 'app-dashboard',
@@ -24,7 +27,8 @@ export class DashboardStaffComponent implements OnInit {
     private houseService: HouseService,
     private roomTypeService: RoomTypeService,
     private statusService: StatusService,
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     //Call API: get total of available rooms
@@ -46,6 +50,42 @@ export class DashboardStaffComponent implements OnInit {
     this.houseService.countTotalAvailableHouse().subscribe(data => {
       this.availableHouseNum = data;
     });
+
+    //Create chart objects
+    var myChart = new Chart("myChart", {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
   }
 
 }
