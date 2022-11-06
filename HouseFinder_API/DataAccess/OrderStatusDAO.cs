0fa321adedb062b3004fa6536ccbea1fe22e4f2f
huyenbnhe150346 <huyenbnhe150346@fpt.edu.vn>
2022-11-06 19:49:33 +0700

    [API] Get all order status

--- HouseFinder_API/DataAccess/OrderStatusDAO.cs
+++ HouseFinder_API/DataAccess/OrderStatusDAO.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using BusinessObjects;
+using DataAccess.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,25 @@ namespace DataAccess
 {
     public class OrderStatusDAO
     {
+       //Get list of all orders' statuses
+        public static List<OrderStatusDTO> GetAllOrderStatus()
+        {
+            List<OrderStatusDTO> statuses;
+            try
+            {
+                using (var context = new FUHouseFinderContext())
+                {
+                    //Get by Id, include Address
+                    MapperConfiguration config;
+                    config = new MapperConfiguration(cfg => cfg.AddProfile(new MapperProfile()));
+                    statuses = context.OrderStatuses.ProjectTo<OrderStatusDTO>(config).ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return statuses;
+        }
     }
 }
