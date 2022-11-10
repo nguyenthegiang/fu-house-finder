58c94d8b95430db152a49f52bd9d586fe41b0651
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-11-10 15:03:21 +0700

    [Angular] Add Google Map API key to Environment

--- house-finder-client/src/environments/environment.prod.ts
+++ house-finder-client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   fb_app_id: '790258838897169',
-  gg_maps_api_key: '',
   gg_client: '919349682446-etrauq4d5cluclesaifkcr4bnh4gru2j.apps.googleusercontent.com',
+  google_maps_api_key: 'AIzaSyA2kdqK31EX1fVy5tqENHPnl8XW85QiNC8',
 };
--- house-finder-client/src/environments/environment.ts
+++ house-finder-client/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   fb_app_id: '790258838897169',
-  gg_maps_api_key: '',
   gg_client: '919349682446-etrauq4d5cluclesaifkcr4bnh4gru2j.apps.googleusercontent.com',
+  google_maps_api_key: 'AIzaSyA2kdqK31EX1fVy5tqENHPnl8XW85QiNC8',
 };
 
 /*
