78f722044769f4cc35a6adab95b7dc51f821ea41
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-11-10 16:04:02 +0700

    [Angular] (Service) Add API URL to Environment

--- house-finder-client/src/environments/environment.ts
+++ house-finder-client/src/environments/environment.ts
@@ -8,6 +8,7 @@ export const environment = {
   fb_app_id: '790258838897169',
   gg_client: '919349682446-etrauq4d5cluclesaifkcr4bnh4gru2j.apps.googleusercontent.com',
   google_maps_api_key: 'AIzaSyA2kdqK31EX1fVy5tqENHPnl8XW85QiNC8',
+  api_url: 'https://localhost:5001/api/',   //URL for Back-end API
 };
 
 /*
