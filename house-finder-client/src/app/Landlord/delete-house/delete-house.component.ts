5ae584d068791e178ab74a8eb9c032c2addc23e1
kienntfs1013 <63889377+kienntfs1013@users.noreply.github.com>
2022-11-11 15:11:24 +0700

    (Landlord) Update delete house

--- house-finder-client/src/app/Landlord/delete-house/delete-house.component.ts
+++ house-finder-client/src/app/Landlord/delete-house/delete-house.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { HouseService } from 'src/app/services/house.service';
 
 @Component({
   selector: 'app-delete-house',
   templateUrl: './delete-house.component.html',
   styleUrls: ['./delete-house.component.scss']
 })
-export class DeleteHouseComponent implements OnInit {
+export class DeleteHouseComponent implements OnInit
+{
+  @Input() houseId!: number;
 
-  constructor() { }
+  constructor(private houseService: HouseService)
+  { }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
+
   }
 
+  goBack(): void
+  {
+    window.location.reload();
+  }
+
+  deleteHouse()
+  {
+    console.log(this.houseId);
+    this.houseService.deleteHouse(this.houseId).subscribe(() => this.goBack());
+  }
 }
