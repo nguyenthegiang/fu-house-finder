21251ea98b07401fc8499524c60ddfcd2512b055
kienntfs1013 <63889377+kienntfs1013@users.noreply.github.com>
2022-11-11 15:08:27 +0700

    (Landlord) Add delete-house component

---
+++ house-finder-client/src/app/Landlord/delete-house/delete-house.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-house',
+  templateUrl: './delete-house.component.html',
+  styleUrls: ['./delete-house.component.scss']
+})
+export class DeleteHouseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- house-finder-client/src/app/app.module.ts
+++ house-finder-client/src/app/app.module.ts
@@ -45,6 +45,7 @@ import { StaffNavbarComponent } from './Common/staff-navbar/staff-navbar.compone
 import { StaffLandlordDetailComponent } from './Staff/staff-landlord-detail/staff-landlord-detail.component';
 import { StaffHouseDetailComponent } from './Staff/staff-house-detail/staff-house-detail.component';
 import { StaffRoomDetailComponent } from './Staff/staff-room-detail/staff-room-detail.component';
+import { DeleteHouseComponent } from './Landlord/delete-house/delete-house.component';
 
 @NgModule({
   declarations: [
@@ -85,6 +86,7 @@ import { StaffRoomDetailComponent } from './Staff/staff-room-detail/staff-room-d
     StaffLandlordDetailComponent,
     StaffHouseDetailComponent,
     StaffRoomDetailComponent,
+    DeleteHouseComponent,
   ],
   imports: [
     BrowserModule,
