742058ad5b83e9b4cda0e82b1d36cf60ca25404c
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-11-12 21:29:20 +0700

    [API] (House Detail) Add Commune & District
    CommuneDAO

---
+++ HouseFinder_API/DataAccess/CommuneDAO.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using BusinessObjects;
+using DataAccess.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class CommuneDAO
+    {
+        //For HouseDAO.GetHouseById()
+        public static CommuneDTO GetCommuneById(int CommuneId)
+        {
+            try
+            {
+                using (var context = new FUHouseFinderContext())
+                {
+                    MapperConfiguration config;
+                    config = new MapperConfiguration(cfg => cfg.AddProfile(new MapperProfile()));
+                    return context.Communes.ProjectTo<CommuneDTO>(config)
+                        .Where(commune => commune.CommuneId == CommuneId).FirstOrDefault();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
