2c1788038e9240530f4db39b78636784cad030a6
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-11-13 22:44:54 +0700

    [Angular] (Landlord) Import Excel
    Get Location: Version 1

--- house-finder-client/src/app/Landlord/import/import.component.ts
+++ house-finder-client/src/app/Landlord/import/import.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImportComponent implements OnInit {
 
+  latitude: number = 0;
+  longitude: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getCurrentLocation();
+  }
+
+  getCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+      });
+    }
+    else {
+      alert("Geolocation is not supported by this browser.");
+    }
   }
 
 }
