74a5f029a0eaaacc72117fca7f6e30e3e7e814bb
kienntfs1013 <63889377+kienntfs1013@users.noreply.github.com>
2022-11-15 13:55:29 +0700

    [Angular] (Staff/list-house): Add info for table

--- house-finder-client/src/app/Staff/staff-landlord-detail/staff-landlord-detail-info/staff-landlord-detail-info.component.ts
+++ house-finder-client/src/app/Staff/staff-landlord-detail/staff-landlord-detail-info/staff-landlord-detail-info.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RoomService } from 'src/app/services/room.service';
 
 @Component({
   selector: 'app-staff-landlord-detail-info',
   templateUrl: './staff-landlord-detail-info.component.html',
   styleUrls: ['./staff-landlord-detail-info.component.scss']
 })
-export class StaffLandlordDetailInfoComponent implements OnInit {
+export class StaffLandlordDetailInfoComponent implements OnInit
+{
+  @Input() houseId!: number;
 
-  constructor() { }
+  totallyAvailableRoom: number = 0;
+  partiallyAvailableRoom: number = 0;
+  availableCapacity: number = 0;
 
-  ngOnInit(): void {
+  constructor(private roomService: RoomService)
+  { }
+
+  ngOnInit(): void
+  {
+    this.roomService.countTotallyAvailableRoomByHouseId(this.houseId).subscribe(data => {
+      this.totallyAvailableRoom = data;
+    });
+
+    this.roomService.countPartiallyAvailableRoomByHouseId(this.houseId).subscribe(data => {
+      this.partiallyAvailableRoom = data;
+    });
+
+    this.roomService.countAvailableCapacityByHouseId(this.houseId).subscribe(data => {
+      this.availableCapacity = data;
+    });
   }
-
 }
