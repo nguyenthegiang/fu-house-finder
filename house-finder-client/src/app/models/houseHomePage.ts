8a0fdd0c4d0c438828babfdb68ab7bdeabece6f5
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-11-16 09:50:10 +0700

    [Angular] (Home Page) List House
    Add Model only for Home Page

---
+++ house-finder-client/src/app/models/houseHomePage.ts
@@ -0,0 +1,33 @@
+import { Address } from './address';
+import { ImagesOfHouse } from './imagesOfHouse';
+
+//DTO: Houses but to display in Home Page -> different attributes
+export interface HouseHomePage {
+    houseId: number;
+    houseName: string;
+    information: string;
+    distanceToCampus: number;
+
+    //price
+    powerPrice: number;
+    waterPrice: number;
+    lowestRoomPrice: number;
+    highestRoomPrice: number;
+
+    //utility
+    fingerprintLock: boolean;
+    camera: boolean;
+    parking: boolean;
+
+    //relationship
+    landlordId: string;
+    address: Address;
+    imagesOfHouses?: ImagesOfHouse[];
+    campusId: number;
+    districtId: number;
+    communeId: number;
+    villageId: number;
+
+    //attributes only for Home Page
+    averageRate: number;
+}
