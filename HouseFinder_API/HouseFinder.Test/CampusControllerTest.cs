f0d43c6ef7ccf20f0498cf004c9fd3ee3ddf504a
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-11-25 09:36:33 +0700

    [Test API] Create Project

---
+++ HouseFinder_API/HouseFinder.Test/CampusControllerTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace HouseFinder.Test
+{
+    public class CampusControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
