c63b8c740283ec8201e854617815793b4ba9df74
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-11-25 10:19:49 +0700

    [Test API] (CampusController)
    GetAll - Returns match data

--- HouseFinder_API/HouseFinder.Test/CampusControllerTest.cs
+++ HouseFinder_API/HouseFinder.Test/CampusControllerTest.cs
@@ -1,4 +1,4 @@
-using BusinessObjects;
+﻿using BusinessObjects;
 using DataAccess.DTO;
 using HouseFinder_API.Controllers;
 using Microsoft.AspNetCore.Mvc;
@@ -32,8 +32,13 @@ namespace HouseFinder.Test
 
         #region GetAllCampuses
 
+        /**
+         * Method: GetAllCampuses()
+         * Scenario: None
+         * Expected behavior: Returns ActionResult
+         */
         [Test]
-        public void GetAllCampuses_Returns_OkResult()
+        public void GetAllCampuses_Returns_ActionResult()
         {
             //ARRANGE
             var campusController = new CampusController();
@@ -43,6 +48,32 @@ namespace HouseFinder.Test
 
             //ASSERT
             Assert.IsInstanceOf<ActionResult<IEnumerable<CampusDTO>>>(data);
+        }
+
+        /**
+         * Method: GetAllCampuses()
+         * Scenario: None
+         * Expected behavior: Returns matching result data
+         */
+        [Test]
+        public void GetAllCampus_MatchResult()
+        {
+            //ARRANGE
+            var campusController = new CampusController();
+
+            //ACT
+            var data = campusController.GetAllCampuses();
+
+            //ASSERT
+            Assert.IsInstanceOf<ActionResult<IEnumerable<CampusDTO>>>(data);
+
+            List<CampusDTO> results = data.Value.ToList();
+
+            Assert.AreEqual("FU - Hòa Lạc", results[0].CampusName);
+            Assert.AreEqual("FU - Hồ Chí Minh", results[1].CampusName);
+            Assert.AreEqual("FU - Đà Nẵng", results[2].CampusName);
+            Assert.AreEqual("FU - Cần Thơ", results[3].CampusName);
+            Assert.AreEqual("FU - Quy Nhơn", results[4].CampusName);
         }
 
         #endregion GetAllCampuses
