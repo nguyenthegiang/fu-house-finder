7a2d004b1dc90f0d357946b4462cb7ed0b39f3d0
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-11-25 09:40:04 +0700

    [Test API] Create Project
    Add Project Reference

--- HouseFinder_API/HouseFinder.Test/CampusControllerTest.cs
+++ HouseFinder_API/HouseFinder.Test/CampusControllerTest.cs
@@ -1,9 +1,17 @@
 using NUnit.Framework;
+using System.Transactions;
+
 
 namespace HouseFinder.Test
 {
+    /**
+     * [Test - CampusController]
+     */
+    [TestFixture]
     public class CampusControllerTest
     {
+        private TransactionScope scope;
+
         [SetUp]
         public void Setup()
         {
