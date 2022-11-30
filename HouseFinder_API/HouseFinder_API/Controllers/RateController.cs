877be6270152dc563e71a5d8a965f4704e0b5ce8
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-11-30 15:05:11 +0700

    [API] (House Detail) Create Rate
    Controller

---
+++ HouseFinder_API/HouseFinder_API/Controllers/RateController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessObjects;
+using Repositories;
+using DataAccess.DTO;
+
+namespace HouseFinder_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RateController : ControllerBase
+    {
+        private IRateRepository rateRepository = new RateRepository();
+
+        //POST: api/Rate
+        [HttpPost]
+        public IActionResult CreateRate(int houseId, int star, string comment)
+        {
+            try
+            {
+                //Create Rate from input info
+                Rate rate = new Rate();
+                rate.Star = star;
+                rate.Comment = comment;
+                rate.HouseId = houseId;
+
+                //Get StudentId
+                string userId = HttpContext.Session.GetString("User");
+                if (userId == null)
+                {
+                    //user not logged in => throw error for alert
+                    return Ok(new { Status = 403 });
+                }
+                rate.StudentId = userId;
+
+                //Default information
+                rate.Deleted = false;
+                rate.CreatedDate = DateTime.Now;
+                rate.LastModifiedDate = DateTime.Now;
+                rate.CreatedBy = userId;
+                rate.LastModifiedBy = userId;
+
+                //Add to Database
+                rateRepository.CreateRate(rate);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
