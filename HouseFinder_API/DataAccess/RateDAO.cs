16bf25dc17813cb493b518129bb52902d51c7e76
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-11-30 14:28:56 +0700

    [API] (House Detail) Create Rate
    DAO

---
+++ HouseFinder_API/DataAccess/RateDAO.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using BusinessObjects;
+using DataAccess.DTO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class RateDAO
+    {
+        /**
+         * [House Detail] Guest create new Rate & Comment
+         */
+        public static void CreateRate(Rate rate)
+        {
+            try
+            {
+                using (var context = new FUHouseFinderContext())
+                {
+                    context.Rates.Add(rate);
+                    context.SaveChanges();
+                }
+            } catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
