eafd59bc40a874415e9410292195de100792cc5b
kienntfs1013 <63889377+kienntfs1013@users.noreply.github.com>
2022-11-30 14:26:30 +0700

    Add landlord header component

---
+++ house-finder-client/src/app/Common/landlord-header/landlord-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landlord-header',
+  templateUrl: './landlord-header.component.html',
+  styleUrls: ['./landlord-header.component.scss']
+})
+export class LandlordHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
