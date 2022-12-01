fe73630ed599cb5f171ecc3364be94b15880a84a
kienntfs1013 <63889377+kienntfs1013@users.noreply.github.com>
2022-12-01 21:53:30 +0700

    (Landlord) Update rate house

--- house-finder-client/src/app/Landlord/rate-house/rate-house.component.ts
+++ house-finder-client/src/app/Landlord/rate-house/rate-house.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
+import { Rate } from 'src/app/models/rate';
+import { RateService } from 'src/app/services/rate.service';
 
 @Component({
   selector: 'app-rate-house',
@@ -7,14 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RateHouseComponent implements OnInit {
   replyOn = false;
+  rates: Rate[] = [];
+  reply: string = "";
+  houseId: number = 0;
+  isReply: boolean = false;
+  indexReply: number = -1;
 
-  constructor() { }
+  @ViewChild('rateSuccessAlert') private rateSuccessAlert: SwalComponent | undefined;
+  @ViewChild('rateErrorAlert') private rateErrorAlert: SwalComponent | undefined;
+
+  constructor(private route: ActivatedRoute,
+    private rateService: RateService,
+    private router: Router)
+  { }
 
   ngOnInit(): void {
+    //Get id of House from Route
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.houseId = id;
+
+    this.rateService.getListRatesByHouseId(this.houseId).subscribe(data => {
+      this.rates = data;
+    });
   }
 
   logout()
   {
     window.location.href = "/login";
+  }
+
+  openReply(index: number)
+  {
+    this.isReply = true;
+    this.indexReply = index;
+  }
+
+  closeReply()
+  {
+    this.isReply = false;
+    this.indexReply = -1;
+  }
+
+  addReply()
+  {
+
   }
 }
