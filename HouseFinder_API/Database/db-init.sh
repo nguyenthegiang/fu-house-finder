a249154288465b1dc7c69219c6afacacfc263189
Nguyen Thu An <annthe150432@gmail.com>
2022-12-05 13:59:55 +0700

    db

---
+++ HouseFinder_API/Database/db-init.sh
@@ -0,0 +1,3 @@
+sleep 20
+
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P h9uHBC8h98h@3snds -i Database.sql
--- HouseFinder_API/Database/entrypoint.sh
+++ HouseFinder_API/Database/entrypoint.sh
@@ -1,7 +1,3 @@
-/opt/mssql/bin/sqlservr
-
-# Wait to be sure that SQL Server came up
-sleep 90s
-# Run the setup script to create the DB and the schema in the DB
-# Note: make sure that your password matches what is in the Dockerfile
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P h9uHBC8h98h@3snds -d master -i Database.sql
+set -m
+/usr/src/app/db-init.sh & /opt/mssql/bin/sqlservr
+fg
