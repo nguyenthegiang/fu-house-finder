ddb5437b8b9aeca19344d3c50103b5b58e556e3e
Nguyen Thu An <annthe150432@gmail.com>
2022-12-05 11:56:41 +0700

    db docker

--- HouseFinder_API/Database/db-init.sh
+++ HouseFinder_API/Database/db-init.sh
@@ -1,6 +1,5 @@
 # Wait to be sure that SQL Server came up
 sleep 90s
-
 # Run the setup script to create the DB and the schema in the DB
 # Note: make sure that your password matches what is in the Dockerfile
-/opt/mssql-tools/bin/sqlcmd -S db -U sa -P S3cur3P@ssW0rd! -d master -i Database.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P h9uHBC8h98h@3snds -d master -i Database.sql
