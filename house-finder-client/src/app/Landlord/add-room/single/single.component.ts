05988060e45dba8f4af9c25bfab3a2605130310a
Nguyen Thu An <42872661+annthe150432@users.noreply.github.com>
2022-12-08 15:19:25 +0700

    apigw docker

--- house-finder-client/src/app/Landlord/add-room/single/single.component.ts
+++ house-finder-client/src/app/Landlord/add-room/single/single.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RoomService } from 'src/app/services/room.service';
 
 @Component({
   selector: 'app-single',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private roomService: RoomService) { 
+    
+  }
 
   ngOnInit(): void {
   }
