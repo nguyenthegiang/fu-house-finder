309e10afb04362d33468dda8ac748ea856e4c270
huyenbnhe150346 <huyenbnhe150346@fpt.edu.vn>
2022-12-11 00:53:13 +0700

    Update list-landlord.component.ts
    Reload list of landlords after update their status

--- house-finder-client/src/app/Staff/list-landlord/list-landlord.component.ts
+++ house-finder-client/src/app/Staff/list-landlord/list-landlord.component.ts
@@ -37,16 +37,7 @@ export class ListLandlordComponent implements OnInit
 
   ngOnInit(): void
   {
-    this.userService.getLandlords().subscribe(data => {
-      this.landlords = data;
-    });
-
-    this.lanlord_informationService.getLandLordInfomation(this.landlordId).subscribe(data => {
-      this.houseCount = data.houseCount;
-      this.roomCount = data.roomCount;
-      this.roomAvailableCount = data.roomAvailableCount;
-    });
-    console.log(this.searchName);
+    this.reloadListLandlord();
   }
 
 
@@ -70,18 +61,30 @@ export class ListLandlordComponent implements OnInit
     });
   }
 
+  reloadListLandlord(){
+    this.userService.getLandlords().subscribe(data => {
+      this.landlords = data;
+    });
+
+    this.lanlord_informationService.getLandLordInfomation(this.landlordId).subscribe(data => {
+      this.houseCount = data.houseCount;
+      this.roomCount = data.roomCount;
+      this.roomAvailableCount = data.roomAvailableCount;
+    });
+  }
+
   updateUserStatus(event: any, userId: string){
     //check if staff just checked or unchecked the checkbox
     const isChecked = (<HTMLInputElement>event.target).checked;
 
     if(isChecked){
       this.selectedStatusId = 1;
-      console.log("Select status: active;" );
     }
     else{
       this.selectedStatusId = 0;
-      console.log("Select status: inactive;" );
     }
-    this.userService.updateUserStatus(userId, this.selectedStatusId).subscribe();
+    this.userService.updateUserStatus(userId, this.selectedStatusId).subscribe((data) => {
+      this.reloadListLandlord();
+    });
   }
 }
