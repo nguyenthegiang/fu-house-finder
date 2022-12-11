f10678f4121118897ae2842230cec8a8278c20f4
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-12-11 17:51:26 +0700

    [Angular] Update Google Map API Key

--- house-finder-client/src/environments/environment.ts
+++ house-finder-client/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: false,
   fb_app_id: '790258838897169',
   gg_client: '919349682446-etrauq4d5cluclesaifkcr4bnh4gru2j.apps.googleusercontent.com',
-  google_maps_api_key: 'AIzaSyAOSt-MODiWy8Tysx0NYkiZ8Ewz1PJkj_M',
+  google_maps_api_key: 'AIzaSyD8_2iKwsk5NE_VDkfU6xIeevqxwU2-hbU',
   api_url: 'https://localhost:5001/api',   //URL for Back-end API
 };
 
