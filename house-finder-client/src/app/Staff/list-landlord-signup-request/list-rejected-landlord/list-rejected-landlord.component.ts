8ec77307a55646e1966ffacab2d14748ff71589e
huyenbnhe150346 <huyenbnhe150346@fpt.edu.vn>
2022-12-12 22:27:29 +0700

    Add component: list-rejected-landlord

---
+++ house-finder-client/src/app/Staff/list-landlord-signup-request/list-rejected-landlord/list-rejected-landlord.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-rejected-landlord',
+  templateUrl: './list-rejected-landlord.component.html',
+  styleUrls: ['./list-rejected-landlord.component.scss']
+})
+export class ListRejectedLandlordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
