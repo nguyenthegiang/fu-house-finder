612bb2ff40cfa8974a0fdad5cb225a168b71fe23
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-12-28 03:50:54 +0700

    [Angular] (Home Page) Confirm Order
    open modal on load

--- house-finder-client/src/app/Guest/confirm-order/confirm-order.component.ts
+++ house-finder-client/src/app/Guest/confirm-order/confirm-order.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-confirm-order',
   templateUrl: './confirm-order.component.html',
   styleUrls: ['./confirm-order.component.scss']
 })
-export class ConfirmOrderComponent implements OnInit {
+export class ConfirmOrderComponent implements OnInit, AfterViewInit {
+  @ViewChild('modalButton') modalButton: ElementRef<HTMLElement> | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    let el: HTMLElement = this.modalButton!.nativeElement;
+    el.click();
   }
 
 }
